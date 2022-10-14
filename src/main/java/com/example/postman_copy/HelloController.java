f9436342e8c664951ea80de3547b934e6b977d66
Halász Gábor <h763416@stud.u-szeged.hu>
2022-10-15 00:28:17 +0200

    Add key value input field when the request type is post Todo: Make possible to add more key value pair dynamically

--- src/main/java/com/example/postman_copy/HelloController.java
+++ src/main/java/com/example/postman_copy/HelloController.java
@@ -16,7 +16,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.scene.layout.Pane;
 
 public class HelloController implements Initializable {
 
@@ -27,51 +31,71 @@ public class HelloController implements Initializable {
     private ChoiceBox<String> requestType;
 
     @FXML
-    private Button sendRequestButton;
+    private TextArea responseText;
 
     @FXML
-    private TextArea responseText;
+    private TextField key1;
+
+    @FXML
+    private TextField value1;
+
+    @FXML
+    private Pane postPanel;
+
 
     @FXML
     protected void sendHttpRequest() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = null;
 
-        if(requestType.getValue() == "Get") {
-            request = HttpRequest.newBuilder()
-                    .uri(URI.create(endpoint.getText()))
-                    .build();
-        } else if(requestType.getValue() == "Post"){
-            // Get key value pairs from the user if the request type is post
-            var values = new HashMap<String, String>() {{
-                put("name", "John Doe");
-                put ("occupation", "gardener");
-            }};
+        if(!endpoint.getText().isEmpty()) {
+            if (requestType.getValue() == "Get") {
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(endpoint.getText()))
+                        .build();
+            } else if (requestType.getValue() == "Post") {
+                // Get key value pairs from the user if the request type is post
+                HashMap values = new HashMap<String, String>() {{
+                    put(key1.getText(), value1.getText());
+                    put("occupation", "gardener");
+                }};
 
-            var objectMapper = new ObjectMapper();
-            String requestBody = objectMapper
-                    .writeValueAsString(values);
+                var objectMapper = new ObjectMapper();
+                String requestBody = objectMapper
+                        .writeValueAsString(values);
 
-            request = HttpRequest.newBuilder()
-                    .uri(URI.create(endpoint.getText()))
-                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                    .build();
+                request = HttpRequest.newBuilder()
+                        .uri(URI.create(endpoint.getText()))
+                        .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                        .build();
+            }
         }
 
         // Public api endpoint for get and post requests
         // http://webcode.me
         // https://httpbin.org/post
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
+        if(request != null) {
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
 
-        responseText.setText("HTTP response is: " + response.body());
+            responseText.setText("HTTP response is: \n" + response.body());
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> requestTypes = FXCollections.observableArrayList(
-                "Get", "Post", "Delete");
+                "Get", "Post");
 
+        requestType.valueProperty().addListener(new ChangeListener<String>() {
+            @Override public void changed(ObservableValue ov, String t, String t1) {
+                if(t1 == "Post") {
+                    postPanel.setVisible(true);
+                } else {
+                    postPanel.setVisible(false);
+                }
+            }
+        });
         requestType.setItems(requestTypes);
         requestType.setValue("Get");
     }
